db751f5cc16323eabae0d2984299d6a1050c99a4
Ruben Schmidt Mällberg <rubschmi@gmail.com>
2016-02-25 21:52:19 +0100

    renamed bookingitem endpoint to bookingitems

--- kordebooking/urls.py
+++ kordebooking/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     url(r'^bookings$', views.BookingList.as_view(), name='booking-list'),
     url(r'^bookings/(?P<pk>\d+)$', views.BookingDetail.as_view(), name='booking-detail'),
 
-    url(r'^bookingitem$', views.BookingItemList.as_view(), name='booking-item-list'),
-    url(r'^bookingitem/(?P<pk>\d+)$', views.BookingItemDetail.as_view(), name='booking-item-detail'),
+    url(r'^bookingitems$', views.BookingItemList.as_view(), name='booking-item-list'),
+    url(r'^bookingitems/(?P<pk>\d+)$', views.BookingItemDetail.as_view(), name='booking-item-detail'),
 ]
